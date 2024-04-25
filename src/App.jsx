24ce2c1341feb137b6888eb3ce8121da608de165
mucopivoine <pivotmurerwa1@gmail.com>
2024-04-25 15:26:52 +0200

    auth outlets

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import './App.css'
 import Homepage from './components/Homepage';
 import Signup from './Pages/Signup';
 import Signin from './Pages/Signin';
-
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import AuthRoute from './Pages/AuthRoute';
 
 
 function App() {
@@ -14,9 +17,17 @@ function App() {
     <>
     <Router>
       <Routes>
-        <Route path="/" element={<Signin/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/homepage" element={<Homepage/>}/>
+        <Route path='/' element={<AuthRoute/>} >
+          <Route path="/" element={<Signin/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Route>
+
+        <Route path="/" element={<Homepage/>}>
+          <Route exact path="/homepage" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Route>
+
       </Routes>
     </Router>
   
